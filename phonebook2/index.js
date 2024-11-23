088db5dc62b2493dbe15fc4afdccd31f868ae8b8
Juuso <juusosavolainen1@kamk.fi>
2024-11-23 19:22:22 +0200

    Done with 3.18

--- phonebook2/index.js
+++ phonebook2/index.js
@@ -4,26 +4,24 @@ const morgan = require('morgan')
 const cors = require('cors')
 require('dotenv').config()
 const Person = require('./models/person')
+
+// Middlewares
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
+// Morgan
 morgan.token('req-body', (req) => {
     return JSON.stringify(req.body);
 })
-
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :req-body'))
-
-let persons = []
-
-Person.find({}).then(person => {
-    persons.push(person)
-})
 
 // INFO page
 app.get('/info', (request, response) => {
-    Person.find({}).then(person => {persons.push(person)})
-    response.send(`<div><p>Phonebook has info for ${persons.length} people</p><br /><p>${Date()}</p></div>`)
+    Person.countDocuments({})
+    .then(count => {
+        response.send(`<div><p>Phonebook has info for ${count} people</p><br /><p>${Date()}</p></div>`)
+    })
 })
 
 // GET all persons
@@ -44,9 +42,9 @@ app.get('/api/persons/:id', (request, response) => {
 
 // DELETE a person
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(p => p.id !== id)
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+    .then(result => {response.status(204).end()})
+    .catch(error => next(error))
 })
 
 // POST a person
@@ -70,19 +68,38 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    // const findPerson = persons.find(p => p.name === person.name)
-
-    // if (findPerson) {
-    //     return response.status(400).json({
-    //         error: 'name must be unique'
-    //     })
-    // }
-    
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
 })
 
+// UPDATE a person
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+    Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(response.status(200).end())
+    .catch(error => next(error))
+})
+
+// Unkown endpoint
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: "unknown endpoint"})
+}
+app.use(unknownEndpoint)
+
+//ErrorHandler
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } 
+    next(error)
+  }
+  app.use(errorHandler)
 
 app.listen(3000, '0.0.0.0', () => {
     console.log('Server is running on 0.0.0.0:3000');
